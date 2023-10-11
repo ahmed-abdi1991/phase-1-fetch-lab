790376a14412324987a07561ebe7902028e47f79
ahmed-abdi1991 <squarehajji@gmail.com>
2023-10-11 18:14:51 +0300

    assignment

--- index.js
+++ index.js
@@ -15,3 +15,52 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// Function to fetch books from the API
+function fetchBooks() {
+  const apiUrl = 'https://anapioficeandfire.com/api/books';
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Call a function to render the books into the DOM
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+// Function to render books into the DOM
+function renderBooks(books) {
+  const bookList = document.getElementById('book-list'); // Assuming you have an element with the id 'book-list' to render the books
+
+  // Clear any previous content
+  bookList.innerHTML = '';
+
+  // Iterate through the books and create list items for each
+  books.forEach(book => {
+    const listItem = document.createElement('li');
+    listItem.textContent = book.name;
+    bookList.appendChild(listItem);
+  });
+}
+
+// Call the fetchBooks() function to fetch and render books when needed
+fetchBooks();
+// Function to render book titles
+function renderBooks(books) {
+  const container = document.getElementById('bookContainer');
+
+  // Iterate through the JSON object and create HTML elements
+  books.forEach(book => {
+      const bookElement = document.createElement('p');
+      bookElement.textContent = book.title;
+      container.appendChild(bookElement);
+  });
+}
